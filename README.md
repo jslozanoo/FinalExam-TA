99327b8b0a5a3c6231d18fb5c6f0f7c3372d3f20
js.lozano@globant.com <142308Jslp>
2022-05-12 10:19:14 -0500

    Add readme.md file and Driver class

---
+++ README.md
@@ -0,0 +1,4 @@
+# FINAL EXAM TEST AUTOMATION ACADEMY
+
+Final exam with avianca page for test automation academy in march 2022.
+Avianca url: https://www.avianca.com/co/en/
---
+++ src/test/java/com/automation/web/driver/Driver.java
@@ -0,0 +1,30 @@
+package com.automation.web.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+
+    private WebDriver driver;
+
+    public Driver(String browser){
+        switch (browser){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
