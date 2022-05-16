6207e5248fdabc0181741b3649e1b7d1b47fa384
js.lozano@globant.com <142308Jslp>
2022-05-16 10:31:06 -0500

    Home page added

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage{
+
+    /**
+     * Constructor
+     *
+     * @param driver WebDriver
+     */
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
