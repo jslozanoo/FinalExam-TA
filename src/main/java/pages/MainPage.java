b56ec876327b57d3f970137d498fb8583f356a29
js.lozano@globant.com <142308Jslp>
2022-05-16 13:03:32 -0500

    Updated javadoc, create an account flow added to exampleTest

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,6 +2,10 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Page representing main page obtained after login or create an account.
+ * @author js.lozano
+ */
 public class MainPage extends BasePage{
 
     /**
