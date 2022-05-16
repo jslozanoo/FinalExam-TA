c2ab3fd70c7ec4a36222bfafbb38e00c03cc7199
js.lozano@globant.com <142308Jslp>
2022-05-16 09:47:33 -0500

    Setted url page

--- src/test/java/com/automation/web/tests/ExampleTest.java
+++ src/test/java/com/automation/web/tests/ExampleTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class ExampleTest extends BaseTest {
 
     @Test
-    public void exampleTest(){
+    public void exampleTest() throws InterruptedException {
+        Thread.sleep(5000);
     }
 }
